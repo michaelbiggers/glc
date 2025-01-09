8d27d86b6ca487dc7dfa919cde96b822fe23526f
Michael <michaelbiggers137@gmail.com>
2025-01-08 22:18:31 -0500

    feat: update Hero component

--- glc-app/app/components/Hero.tsx
+++ glc-app/app/components/Hero.tsx
@@ -1,16 +1,27 @@
+import Image from "next/image";
+import group from "@/public/group.jpg";
 export default function Hero() {
   /**
    * props: height, bg url, bg position, bg opacity
    */
   return (
     <div className="hero relative min-h-screen flex justify-center items-center">
-      <div className="hero__bg absolute bg-center bg-cover bg-[url('/organic.svg')] top-0 left-0 right-0 bottom-0 -z-1 opacity-20"></div>
-      <div className="hero__text-container relative text-center max-w-prose">
-        <h1 className=" text-6xl">Your Experts in Communication and Learning Disorders</h1>
-        <p className = "text-xl">Speech-Language Therapy & Psychoeducational Services</p>
-        <a href="" className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded-full">Who we work with</a>
+      <div className="hero__bg bg-white px-24 py-16 m-36 rounded-2xl">
+        <h1 className="text-center text-5xl">Your Experts in Communication and Learning Disorders</h1>
+        <div className="flex justify-center items-center">
+          <div className="hero__img-container basis-1/2 ">
+            <Image
+              src={group}
+              alt='Sunny Biggers and Jacie Curry, owners of Greenville Learning Collective'
+              className="rounded-2xl z-1"
+            />
+          </div>
+          <div className="hero__text-container basis-1/2 p-12">
+            <p className = "mb-8">We are professionals who specialize in evaluating and treating a variety of disorders, including communication disorders, autism, ADHD, learning disabilities (including dyslexia), and developmental delays.</p>
+            <a href="" className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded-full">Who we work with</a>
+          </div>
+        </div>
       </div>
-
     </div>
   );
 }
--- glc-app/app/page.tsx
+++ glc-app/app/page.tsx
@@ -1,9 +1,38 @@
 import Hero from "./components/Hero";
-
+import Image from "next/image";
+import group from '@/public/group.jpg'
 export default function Home() {
   return (
-    <div>
+    <div className = "z-1 relative">
       <Hero />
+      {/* <div className="team-photo max-w-5xl m-auto -mt-24 ">
+        <Image
+          src={group}
+          alt='Sunny Biggers and Jacie Curry, owners of Greenville Learning Collective'
+          className="rounded-3xl z-1"
+        />
+      </div> */}
+      <div className="icon-bar flex max-w-screen w-9/12 justify-center items-center m-auto -mt-12 bg-white absolute center gap-12 p-12 left-1/2 -translate-x-1/2">
+        <div className="flex flex-col justify-center items-center">
+          <i>icon</i>
+          <h3>Accepting new clients</h3>
+        </div>
+        <div className="flex flex-col justify-center items-center">
+          <i>icon</i>
+          <h3>Telehealth or in person appointments</h3>
+        </div>
+        <div className="flex flex-col justify-center items-center">
+          <i>icon</i>
+          <h3>Accepts online payments</h3>
+        </div>
+      </div>
+      {/* <div className="team bg-black min-h-screen">
+        <Image
+          src={group}
+          alt='Sunny Biggers and Jacie Curry, owners of Greenville Learning Collective'
+          className="rounded-3xl z-1"
+        />
+      </div> */}
     </div>
   );
 }
