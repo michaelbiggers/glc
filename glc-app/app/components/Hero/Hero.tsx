7892e57163262a154ba08ef8692b2b5f4c7f9494
Michael <michaelbiggers137@gmail.com>
2025-05-01 22:33:24 -0400

    feat: refactor HeroBackground into wrapper component

--- glc-app/app/components/Hero/Hero.tsx
+++ glc-app/app/components/Hero/Hero.tsx
@@ -1,6 +1,6 @@
 import { StaticImageData } from "next/image";
-import { Button } from "./Button";
 import { ReactNode } from "react";
+import HeroBackground from "./HeroBackground";
 type HeroProps = {
   backgroundImage: string | StaticImageData;
   backgroundPosition?: React.CSSProperties['backgroundPosition'];
@@ -8,21 +8,14 @@ type HeroProps = {
   position?: 'start' | 'center' | 'end';
   children?: ReactNode;
 };
-export default function Hero({ backgroundImage, backgroundPosition = "center center", backgroundSize = "cover", position = "start", children }: HeroProps) {
-  const backgroundUrl = typeof backgroundImage === 'string' ? backgroundImage : backgroundImage.src;
-  const positionClass = {
-    start: 'justify-start',
-    center: 'justify-center',
-    end: 'justify-end',
-  }[position];
+export default function Hero({
+  backgroundImage,
+  backgroundPosition = "center center",
+  backgroundSize = "cover",
+  position = "start",
+  children }: HeroProps) {
   return (
-    <div className={`hero relative min-h-[80vh] flex items-center bg-[#faf5e8] ${positionClass}`}
-      style={
-        {
-          backgroundImage: `url('${backgroundUrl}')`,
-          backgroundSize: backgroundSize,
-          backgroundPosition: backgroundPosition
-        }}>
+    <HeroBackground backgroundImage={backgroundImage} backgroundPosition={backgroundPosition} backgroundSize={backgroundSize} position={position}>
       <div className="hero__overlay bg-[#fffaf6] py-16 m-8 lg:m-36 rounded-2xl w-full lg:max-w-xl">
         <div className="flex justify-center items-center">
           <div className="hero__text-container p-12">
@@ -30,6 +23,6 @@ export default function Hero({ backgroundImage, backgroundPosition = "center cen
           </div>
         </div>
       </div>
-    </div>
+    </HeroBackground >
   );
 }
