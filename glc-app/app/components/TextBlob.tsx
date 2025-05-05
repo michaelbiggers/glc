a70bc10b92380f12ea0ab7dcf3829b266a4b8f2e
Michael <michaelbiggers137@gmail.com>
2025-05-05 15:40:41 -0400

    feat: reduce TextBlob padding

--- glc-app/app/components/TextBlob.tsx
+++ glc-app/app/components/TextBlob.tsx
@@ -13,7 +13,7 @@ export const TextBlob = ({ color = 'yellow', fullWidth = true, children, classNa
   };
   const width = fullWidth ? 'w-full': 'w-[90vw] m-auto';
   return (
-    <div className={`py-12 lg:py-36 px-12 lg:px-48 rounded-[48px] ${variants[color]} ${width} ${classNames}`}>
+    <div className={`py-12 lg:py-28 px-12 lg:px-48 rounded-[48px] ${variants[color]} ${width} ${classNames}`}>
       <div className="">
         {children}
       </div>
