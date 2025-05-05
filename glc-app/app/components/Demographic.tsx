7b7d89407fc6ddb926e86a8bffee3561ca87a527
Michael <michaelbiggers137@gmail.com>
2025-05-05 14:44:32 -0400

    feat: small Demographic improvements

--- glc-app/app/components/Demographic.tsx
+++ glc-app/app/components/Demographic.tsx
@@ -12,7 +12,7 @@ const Demographic = ({id = ''}:DemographicProps) => {
           <h4 className="uppercase font-bold mb-2 mt-8">How We Can Help</h4>
           <p className="text-2xl mb-8">We specialize in evaluating and treating a variety of disorders, including the following:</p>
           <div className="lg:columns-2 ">
-            <ul className="text-xl">
+            <ul className="text-xl list-disc ms-8">
               <li>ADD/ADHD</li>
               <li>Anxiety</li>
               <li>Articulation Disorders</li>
@@ -24,7 +24,6 @@ const Demographic = ({id = ''}:DemographicProps) => {
               <li>Dysarthria</li>
               <li>Executive Functioning Disorders</li>
               <li>Fluency (stuttering/cluttering)</li>
-              <li>Free Consultation</li>
               <li>Individual/Group Speech Therapy</li>
               <li>Language Processing Disorders</li>
               <li>Learning Disabilities</li>
