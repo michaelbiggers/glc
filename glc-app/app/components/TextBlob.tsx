979399201430cbd160f42ce17f49b68d3a639658
Michael <michaelbiggers137@gmail.com>
2025-05-05 19:56:52 -0400

    feat: tighten up TextBlob padding

--- glc-app/app/components/TextBlob.tsx
+++ glc-app/app/components/TextBlob.tsx
@@ -5,18 +5,16 @@ type TextBlobProps = {
   children?: ReactNode;
   classNames?: string;
 };
-export const TextBlob = ({ color = 'yellow', fullWidth = true, children, classNames = ''}: TextBlobProps) => {
+export const TextBlob = ({ color = 'yellow', fullWidth = true, children, classNames = '' }: TextBlobProps) => {
   const variants = {
     yellow: "bg-[#f2ae21]",
     orange: "bg-[#d14d22]",
     green: "bg-[#39747f]",
   };
-  const width = fullWidth ? 'w-full': 'w-[90vw] m-auto';
+  const width = fullWidth ? 'w-full' : 'w-[90vw] m-auto';
   return (
     <div className={`py-12 lg:py-28 px-12 lg:px-48 rounded-[48px] ${variants[color]} ${width} ${classNames}`}>
-      <div className="">
-        {children}
-      </div>
+      {children}
     </div>
 
   );
