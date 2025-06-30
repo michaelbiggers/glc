b202dd31c91f4e4a6869711ddabc1a3444609ecb
Michael <michaelbiggers137@gmail.com>
2025-06-30 16:36:01 -0400

    feat: update faqs

--- glc-app/app/components/Faqs.tsx
+++ glc-app/app/components/Faqs.tsx
@@ -9,10 +9,10 @@ const Faqs = () => {
       <div className="faqs__col basis-full lg:basis-2/3">
         <Faq
           title="What is the process to get started? What can I expect?"
-          content="Call us at 864-451-2563 to discuss any questions or concerns you may have. You can also book a free consultation appointment online. Just click the Request Appointment button to see current availability. " />
+          content="Our process typically begins with a free phone or virtual consultation. This appointment can be booked online, Just click the Request Appointment button to see current availability. You can also give us a call at 864-451-2563. From there, we will guide you through the next steps. Typically, intake paperwork is completed through our patient portal. You will schedule one or two evaluation appointments to meet with our clinicians in-office. Expect to be at the evaluation appointment for 1-2 hours." />
         <Faq
           title="Do you accept insurance?">
-          We accept a range of insurance providers, as well as self-pay options. Please check our <Link href = '/pricing' style={{textDecoration: 'underline'}}>insurance page</Link> for an up-to-date list of providers.
+          We accept a range of insurance providers, as well as self-pay options. Please check our <Link href='/pricing' style={{ textDecoration: 'underline' }}>insurance page</Link> for an up-to-date list of providers.
         </Faq>
         <Faq
           title="How long will it take to get an appointment? "
@@ -22,10 +22,13 @@ const Faqs = () => {
           content="Yes. If you are seeking an evaluation or therapy using any form of insurance a physician's referral is needed. A prior authorization for service may also be required. If you are a private-pay client, no referral is necessary. " />
         <Faq
           title="Does my child need an evaluation and/or therapy?"
-          content="After speaking with one of our clinician’s we will advise you on the necessity of an evaluation and how long the process will take. After your child is evaluated we will write a report with recommendations. A virtual or in-person meeting will be scheduled to review the evaluation results and recommendations. " />
+          content="After speaking with one of our clinicians, we will advise you on the necessity of an evaluation and how long the process will take. After your child is evaluated, we will write a report with recommendations. A virtual or in-person meeting will be scheduled to review the evaluation results and recommendations." />
         <Faq
           title="What if I already have an evaluation from another provider?"
           content="We will review any recent evaluation completed within the past 6 months and determine if an updated evaluation is warranted." />
+        <Faq
+          title="Is GLC able to provide a medical diagnosis of Autism?"
+          content="Yes! Our evaluations meet standards for Medicaid, state services, and most insurance companies. " />
       </div>
     </div>
   );
